53cafc6336324c12611d763297bdb2b9c1c73a0a
tji01 <tyleri.18j@gmail.com>
2026-08-04 08:47:20 -0400

    Bug

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -34,7 +34,7 @@ router.post('/signup', async (req, res) => {
 	//insert user info to database
 	const result = await pool.query('INSERT INTO users (email, password_hash, name) VALUES ($1, $2, $3) RETURNING id, email, name', [email, pass, name]);
 
-	res.status(201).json({user: result.rows([0])});
+	res.status(201).json({user: result.rows[0]});
 
 });
 
